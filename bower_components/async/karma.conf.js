dc00244ecbb93d2ad27fb759dd4874abb7cd27c2
Walterleonardo <walterleonardo@gmail.com>
2017-06-15 09:23:54 +0200

    commit all the directory

--- bower_components/async/karma.conf.js
+++ bower_components/async/karma.conf.js
@@ -1,12 +1,12 @@
 module.exports = function (config) {
-  config.set({
-    browsers: ['Firefox'],
-    files: ['mocha_test/*.js'],
-    frameworks: ['browserify', 'mocha'],
-    preprocessors: {
-      'mocha_test/*.js': ['browserify']
-    },
-    reporters: ['mocha'],
-    singleRun: true
-  });
+    config.set({
+        browsers: ['Firefox'],
+        files: ['mocha_test/*.js'],
+        frameworks: ['browserify', 'mocha'],
+        preprocessors: {
+            'mocha_test/*.js': ['browserify']
+        },
+        reporters: ['mocha'],
+        singleRun: true
+    });
 }
